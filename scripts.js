1f828fe6a5a90aef3f6adcb0a933683627476ca6
JLCurtsinger <98992595+JLCurtsinger@users.noreply.github.com>
2024-04-25 22:18:07 -0700

    added photo gallery
    still working on it

--- scripts.js
+++ scripts.js
@@ -15,3 +15,26 @@ function changeItem(index) {
 document.addEventListener("DOMContentLoaded", () => {
     changeItem(0);
 });
+
+// photo gallery
+const swiper = new Swiper('.swiper', {
+    // Optional parameters
+    direction: 'vertical',
+    loop: true,
+  
+    // If we need pagination
+    pagination: {
+      el: '.swiper-pagination',
+    },
+  
+    // Navigation arrows
+    navigation: {
+      nextEl: '.swiper-button-next',
+      prevEl: '.swiper-button-prev',
+    },
+  
+    // And if we need scrollbar
+    scrollbar: {
+      el: '.swiper-scrollbar',
+    },
+  });
